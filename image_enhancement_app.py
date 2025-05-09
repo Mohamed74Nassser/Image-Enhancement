dbe185ffecd93cbcd9af85d2ff15549623fc22a6
ekhlassobhy <ekhlassobhy632@gmail.com>
2025-05-09 19:08:46 +0300

    create adjust_brightness function

--- image_enhancement_app.py
+++ image_enhancement_app.py
@@ -5,3 +5,7 @@ from PIL import Image
 
 # Set Streamlit layout to wide for better image display
 st.set_page_config(layout="wide")
+
+# Adjust image brightness by adding a constant value (beta)
+def adjust_brightness(image, beta):
+    return cv2.convertScaleAbs(image, alpha=1, beta=beta)
